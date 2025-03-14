0bedf143576ce0865215230dbf990a52c7379fa8
zc <z304307393@163.com>
2025-03-14 18:26:22 +0800

    fix: iframe not delete

--- src/editor/components/ReactIframe.tsx
+++ src/editor/components/ReactIframe.tsx
@@ -1,10 +1,6 @@
-import { useRef, useCallback, useEffect } from 'react'
-import { useLexicalEditable } from '@lexical/react/useLexicalEditable'
-import { useLexicalNodeSelection } from '@lexical/react/useLexicalNodeSelection'
-import { mergeRegister } from '@lexical/utils'
-import { CLICK_COMMAND, COMMAND_PRIORITY_LOW, DRAGSTART_COMMAND } from 'lexical'
-
-import { useEditorContext } from '../hooks'
+import { useRef } from 'react'
+import { BlockWithAlignableContents } from '@lexical/react/LexicalBlockWithAlignableContents'
+import { NodeKey } from 'lexical'
 
 export interface ReactIframeProps {
   src: string
@@ -18,55 +14,12 @@ export function ReactIframe(props: ReactIframeProps) {
 
   const nodeRef = useRef<HTMLIFrameElement>(null)
 
-  const [isSelected, setSelected, clearSelection] = useLexicalNodeSelection(nodeKey)
-  const isEditable = useLexicalEditable()
-  const { activeEditor } = useEditorContext()
-
-  const focused = isSelected && isEditable
-
-  const handleOnClickCommand = useCallback(
-    (event: MouseEvent) => {
-      if (event.target === nodeRef.current) {
-        if (event.shiftKey) {
-          setSelected(!isSelected)
-        } else {
-          clearSelection()
-          setSelected(true)
-        }
-        return true
-      }
-
-      return false
-    },
-    [clearSelection, isSelected, setSelected]
-  )
-
-  useEffect(() => {
-    return mergeRegister(
-      activeEditor.registerCommand(CLICK_COMMAND, handleOnClickCommand, COMMAND_PRIORITY_LOW),
-      activeEditor.registerCommand(
-        DRAGSTART_COMMAND,
-        (event: DragEvent) => {
-          if (event.target === nodeRef.current) {
-            event.preventDefault()
-            return true
-          }
-          return false
-        },
-        COMMAND_PRIORITY_LOW
-      )
-    )
-  }, [activeEditor, handleOnClickCommand])
-
   return (
-    <iframe
-      ref={nodeRef}
-      className={['editor-iframe', focused ? 'editor-iframe-focused' : ''].join(' ')}
-      width='100%'
-      height={height}
-      src={src}
-      allow={elementAllow}
-      draggable={false}
-    />
+    <BlockWithAlignableContents
+      nodeKey={nodeKey as NodeKey}
+      className={{ base: 'editor-iframe', focus: 'editor-iframe-focused' }}
+    >
+      <iframe ref={nodeRef} width='100%' style={{ height }} src={src} allow={elementAllow} draggable={false} />
+    </BlockWithAlignableContents>
   )
 }
