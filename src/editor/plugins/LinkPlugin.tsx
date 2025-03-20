f741cac721b3ca3c6ebce83184038c863d54ef16
zc <z304307393@163.com>
2025-03-20 18:34:04 +0800

    feat: when has some node not trigger link node

--- src/editor/plugins/LinkPlugin.tsx
+++ src/editor/plugins/LinkPlugin.tsx
@@ -1,7 +1,36 @@
 import { LinkPlugin as LexicalLinkPlugin } from '@lexical/react/LexicalLinkPlugin'
+import { $getSelection, $isRangeSelection } from 'lexical'
 
 import { validateUrl } from '../utils/url'
+import { useEditorContext } from '../hooks'
+import { $isIframeNode, $isImageNode, $isMediaNode, $isMentionNode } from '../nodes'
 
 export function LinkPlugin() {
-  return <LexicalLinkPlugin validateUrl={validateUrl} attributes={{ rel: 'noopener noreferrer', target: '_blank' }} />
+  const { activeEditor } = useEditorContext()
+
+  const handleOnValidateUrl = (url: string) => {
+    const nodes = activeEditor.getEditorState().read(() => {
+      const selection = $getSelection()
+      if ($isRangeSelection(selection)) {
+        const nodes = selection.getNodes()
+        return nodes.filter((node) => {
+          return $isImageNode(node) || $isMediaNode(node) || $isIframeNode(node) || $isMentionNode(node)
+        })
+      }
+      return []
+    })
+
+    if (nodes.length > 0) {
+      return false
+    }
+
+    return validateUrl(url)
+  }
+
+  return (
+    <LexicalLinkPlugin
+      validateUrl={handleOnValidateUrl}
+      attributes={{ rel: 'noopener noreferrer', target: '_blank' }}
+    />
+  )
 }
