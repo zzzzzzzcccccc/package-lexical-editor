8fce53677ed353c8a852c61c34ff9e0b4bf16607
zc <z304307393@163.com>
2025-03-31 16:33:05 +0800

    fix: not show error image when load image failed

--- src/editor/hooks/useImageCache.ts
+++ src/editor/hooks/useImageCache.ts
@@ -2,15 +2,16 @@ const imageCache = new Set<string>()
 
 export function useImageCache(target: string) {
   if (!imageCache.has(target)) {
+    imageCache.add(target)
+
     throw new Promise<void>((resolve) => {
       const img = new Image()
       img.src = target
       img.onload = () => {
-        imageCache.add(target)
         resolve()
       }
       img.onerror = () => {
-        imageCache.add(target)
+        resolve()
       }
     })
   }
